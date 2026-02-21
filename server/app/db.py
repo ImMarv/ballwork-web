b98221b885be84912b78d3c531904e5af060621b
Marv <94709651+ImMarv@users.noreply.github.com>
2026-02-21 10:29:34 +0000

    i hate databases

--- server/app/db.py
+++ server/app/db.py
@@ -1,23 +1,13 @@
 """The database file. Managing the engine and transactions here."""
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from .core.settings import settings
-
-
-class Base(DeclarativeBase):
-    pass
-
 
 engine = create_engine(settings.DATABASE_URL)  # connects to the database
 
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def create_db_and_tables():
-    Base.metadata.create_all(bind=engine)
-
 
 def get_db():
     db = SessionFactory()
