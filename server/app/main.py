57a156d62dd029031e37791c13d8f385c5434acf
Marv <94709651+ImMarv@users.noreply.github.com>
2026-02-24 21:12:18 +0000

    fix for lifespan issue

--- server/app/main.py
+++ server/app/main.py
@@ -1,13 +1,13 @@
 from contextlib import asynccontextmanager
 
+from sqlalchemy import text
 import uvicorn
-from fastapi import FastAPI
-
 from app.core.settings import settings
-from app.db import create_db_and_tables, engine
+from app.db import engine
 from app.modules.stats.api import router as stats_router
 from app.modules.stats.providers.api_football import ApiFootballProvider
 from app.modules.stats.service import StatsService
+from fastapi import FastAPI
 
 provider = ApiFootballProvider(api_key=settings.API_FOOTBALL_KEY)
 service = StatsService(provider=provider)
@@ -19,7 +19,8 @@ def get_stats_service() -> StatsService:
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_db_and_tables()
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
     yield
     engine.dispose()
 
