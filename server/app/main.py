0e1698b87394504f41e8feefd887c93516270ec0
Marv <94709651+ImMarv@users.noreply.github.com>
2026-03-07 20:48:53 +0000

    changed prefix to correct one

--- server/app/main.py
+++ server/app/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(stats_router, prefix="/stats")
+app.include_router(stats_router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
