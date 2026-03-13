18ab61d73977ddd5e10a8949c6c0847bad1ba09f
Marv <94709651+ImMarv@users.noreply.github.com>
2026-03-13 21:18:09 +0000

    small change to have mapped column take EntityType enum

--- server/app/modules/digest/repository/models/subscription.py
+++ server/app/modules/digest/repository/models/subscription.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from datetime import datetime
 
 from app.db_base.base import Base
-from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy import DateTime, Enum, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .enums.entity_type import EntityType
@@ -16,6 +16,6 @@ class Subscription(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     subscriber_id: Mapped[int] = mapped_column(ForeignKey("subscriber_digest.id"))
     entity_id: Mapped[int] = mapped_column(index=True)
-    entity_type: Mapped[EntityType] = mapped_column()
+    entity_type: Mapped[EntityType] = mapped_column(Enum(EntityType), nullable=False)
     target_type: Mapped[str] = mapped_column(String(20))
     createdAt: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
