b6ea79855c92731034a8eec1fcd66e1ba2146143
Marv <94709651+ImMarv@users.noreply.github.com>
2026-04-08 12:42:03 +0100

    changed stats service factory now more consistent to other factories

--- server/app/modules/stats/deps.py
+++ server/app/modules/stats/deps.py
@@ -3,21 +3,25 @@ In charge of wiring the local API with the provider
 """
 
 # stats/deps.py
-from app.core.settings import settings
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
+from app.db import get_db
+
+from ...core.settings import settings
 from ..stats.providers.api_football import ApiFootballProvider
 from ..stats.service import StatsService
 
 # Create single instances (application scope)
 _provider = ApiFootballProvider(api_key=settings.API_FOOTBALL_KEY)
-_stats_service = StatsService(_provider)
+_session = get_db()
 
 
-def get_stats_service() -> StatsService:
+def get_stats_service(db: Session = Depends(get_db)) -> StatsService: #noqa: B008
     """
     Gets the header data necessary to request data from the provider
 
     :return: The stats service credentials
     :rtype: StatsService
     """
-    return _stats_service
+    return StatsService(_provider)
